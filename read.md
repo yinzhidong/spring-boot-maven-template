f978c19986c1d5901ff45c1663999717603461d2
zhidongyin <zhidong.yin@chatail.com>
2023-03-21 14:13:55 +0800

    update version

---
+++ read.md
@@ -0,0 +1,14 @@
+
+https://github.com/zeabur/spring-boot-maven-template
+
+https://github.dev/yinzhidong/spring-boot-maven-template
+
+
+https://yinzhidong-curly-computing-machine-9669jpxg4grfpj4q.github.dev/
+
+
+
+https://dash.zeabur.com/projects/641913f5662d5093da1f48fa/services/64191f3b662d5093da1f4974?envID=641913f5783a618d07525b0b
+
+
+> https://docs.zeabur.com/zh-CN
--- src/main/java/com/benson/ZeaburMavenTest/Controller/HelloWorldController.java
+++ src/main/java/com/benson/ZeaburMavenTest/Controller/HelloWorldController.java
@@ -3,10 +3,14 @@ package com.benson.ZeaburMavenTest.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController
 public class HelloWorldController {
+
     @GetMapping("/")
     public String index(){
-        return "Hello Zeabur Maven Project!";
+        return "Hello Zeabur SpringBoot - " + UUID.randomUUID().toString();
     }
+
 }
