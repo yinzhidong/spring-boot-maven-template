6d447ccfae03d9292871090c57f3f1a3fd40bd87
yinzhidong <22864444+yinzhidong@users.noreply.github.com>
2023-03-21 03:24:11 +0000

    update version

--- src/test/java/com/benson/ZeaburMavenTest/ZeaburMavenTestApplicationTests.java
+++ src/test/java/com/benson/ZeaburMavenTest/ZeaburMavenTestApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ZeaburMavenTestApplicationTests {
+public class ZeaburMavenTestApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
